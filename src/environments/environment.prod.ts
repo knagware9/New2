02200e2deca712814107bdcc462e9bdf1d1b7361
adhavpavan <adhavpavan@gmail.com>
2018-04-19 22:35:59 +0530

    Added firebase config to dev

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  FirebaseConfig : {
+    apiKey: "AIzaSyAhIkn6ENusZc5SLA6q4CbSu-Sgt7-tFqo",
+    authDomain: "angular5crud-720aa.firebaseapp.com",
+    databaseURL: "https://angular5crud-720aa.firebaseio.com",
+    projectId: "angular5crud-720aa",
+    storageBucket: "angular5crud-720aa.appspot.com",
+    messagingSenderId: "444756683395"
+  }
 };
